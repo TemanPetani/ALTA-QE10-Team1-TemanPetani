5fcbbfba8283f67d9a26b5c0565012f1b0d24a4f
ghalda <gputribalqis@gmail.com>
2023-06-22 04:59:07 +0700

    setup api automation

---
+++ src/test/java/starter/TemanPetani/TemanPetaniResponse.java
@@ -0,0 +1,9 @@
+package starter.TemanPetani;
+
+public class TemanPetaniResponse {
+    public static String ID = "id";
+    public static String EMAIL = "email";
+    public static String USER_NAME = "username";
+    public static String PASSWORD = "password";
+    public static String FULLNAME = "fullname";
+}
---
+++ src/test/java/starter/Utils/Constant.java
@@ -0,0 +1,8 @@
+package starter.Utils;
+
+public class Constant {
+    public static String BASE_URL="https://temanpetani-api-7hxmz4cxza-as.a.run.app/";
+    public static final String DIR = System.getProperty("user.dir");
+    public static String REQ_BODY_DIR = DIR + "/src/test/resources/JSON/ReqBody/";
+    public static String JSON_SCHEMA_DIR = DIR + "/src/test/resources/JSON/JSONSchema/";
+}
