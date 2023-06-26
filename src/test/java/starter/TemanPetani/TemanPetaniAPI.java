7630dc31ec2a0ddb628cce59a47d32cb0b924aab
ghalda <gputribalqis@gmail.com>
2023-06-26 09:57:47 +0700

    general files added

--- src/test/java/starter/TemanPetani/TemanPetaniAPI.java
+++ src/test/java/starter/TemanPetani/TemanPetaniAPI.java
@@ -1,6 +1,11 @@
 package starter.TemanPetani;
 
+import io.restassured.http.ContentType;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Step;
 import starter.Utils.Constant;
+
+import java.io.File;
 
 public class TemanPetaniAPI {
     public static String LOGIN = Constant.BASE_URL+ "/login";
@@ -9,4 +14,11 @@ public class TemanPetaniAPI {
     public static String LIST_PRODUCT = Constant.BASE_URL+ "/users/products";
     public static String LIST_SOLD = Constant.BASE_URL+ "/users/sold";
     public static String LIST_ORDER = Constant.BASE_URL+ "/users/orders";
+
+    @Step("Post login")
+    public void postLogin(File json){
+        SerenityRest.given()
+                .contentType(ContentType.JSON)
+                .body(json);
+    }
 }
--- src/test/java/starter/TemanPetani/TemanPetaniResponse.java
+++ src/test/java/starter/TemanPetani/TemanPetaniResponse.java
@@ -6,4 +6,7 @@ public class TemanPetaniResponse {
     public static String USER_NAME = "username";
     public static String PASSWORD = "password";
     public static String FULLNAME = "fullname";
+    public static String STATUS = "status";
+    public static String MESSAGE = "message";
+    public static String TOKEN_USERS = "data.token";
 }
